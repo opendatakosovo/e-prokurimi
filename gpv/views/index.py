ea2b5f6bbd7091db3c537ea7bad74bf35869ab6d
Your Name <partin.imeri@gmail.com>
2014-10-22 13:30:37 +0200

    Added HTML and Flask logic to show simple charts

---
+++ gpv/views/index.py
@@ -0,0 +1,14 @@
+from flask import render_template, request, redirect, url_for
+from flask.views import View
+from requests import request
+from urllib2 import Request, urlopen
+import json
+
+class Index(View):
+    def dispatch_request(self):
+        url = "http://0.0.0.0:5000/treemap"
+        table_results = json.loads(urlopen(url).read())
+
+
+
+        return render_template('index.html',table_results=table_results)
---
+++ gpv/views/procurementtype.py
@@ -0,0 +1,14 @@
+from flask import render_template, request, redirect, url_for
+from flask.views import View
+from requests import request
+from urllib2 import Request, urlopen
+import json
+
+class ProcurementType(View):
+    def dispatch_request(self):
+        url = "http://0.0.0.0:5000/piechart"
+        piechart = json.loads(urlopen(url).read())
+
+
+
+        return render_template('procurement_type.html',piechart=piechart)
