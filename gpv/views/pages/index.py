d5996f6848846ee4266de4aa464f30c84396fa88
Georges Labreche <georges@opendatakosovo.org>
2014-10-30 17:35:32 -0700

    Implemented treemap go back link

--- gpv/views/pages/index.py
+++ gpv/views/pages/index.py
@@ -1,12 +1,17 @@
 from flask import render_template
 from flask.views import View
 from urllib2 import urlopen
+from gpv import utils
 import json
 
 
 class Index(View):
     def dispatch_request(self):
-        url = "http://0.0.0.0:5000/monthly-summary"
+    	api_base_url = utils.get_api_url()
+        url = "%s/monthly-summary" % api_base_url
+
+        print url
+
         result_json = json.loads(urlopen(url).read())
 
         return render_template('index.html', result_json=result_json)
