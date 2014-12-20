36241e58b3e7a63b88b909b373e07f77275e93a3
Georges Labreche <georges@opendatakosovo.org>
2014-12-20 09:03:04 -0800

    fixed default home path redirect

--- gpv/views/pages/home.py
+++ gpv/views/pages/home.py
@@ -1,7 +1,7 @@
-from flask import render_template, redirect
+from flask import render_template, redirect, current_app
 from flask.views import View
 
 class Home(View):
     def dispatch_request(self):
-
-        return redirect('/ferizaj')
+    	default_home_path = current_app.config['BASE_PATH'] + '/ferizaj'
+        return redirect(default_home_path)
