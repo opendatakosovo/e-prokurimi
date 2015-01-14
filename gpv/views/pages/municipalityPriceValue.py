a3dd893a3ac47188b3defb0c5c9c7135258cd980
Vullkan92 <vullkan.halili@riinvest.net>
2015-01-14 11:38:57 +0100

    Modified the style of Select box and krahasimi's url.

--- gpv/views/pages/municipalityPriceValue.py
+++ gpv/views/pages/municipalityPriceValue.py
@@ -1,10 +1,8 @@
 from flask import render_template
 from flask.views import View
-from urllib2 import urlopen
-from gpv import utils
-import json
 
 
 class MunicipalityPriceValue(View):
     def dispatch_request(self):
-        return render_template('municipality_price_value.html')
+        komuna = 'ferizaj'
+        return render_template('municipality_price_value.html', komuna=komuna)
