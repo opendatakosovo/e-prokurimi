b5a993dbcf3a928cb4f6339cf0308030508c616d
Your Name <partin.imeri@gmail.com>
2015-01-15 16:01:13 +0100

    Added a default value when no city is selected

--- gpv/views/pages/municipalityPriceValue.py
+++ gpv/views/pages/municipalityPriceValue.py
@@ -5,4 +5,6 @@ from flask.views import View
 class MunicipalityPriceValue(View):
     def dispatch_request(self):
         commune = request.args.get('komuna')
+        if commune == "None":
+            commune = "ferizaj"
         return render_template('municipality_price_value.html', komuna=commune)
