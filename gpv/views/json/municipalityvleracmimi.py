733e1669ec83802f1bf8c61d7b6e2c10ac74863b
Your Name <partin.imeri@gmail.com>
2015-01-15 15:54:31 +0100

    Fixed the URL building when in page Krahasimi

--- gpv/views/json/municipalityvleracmimi.py
+++ gpv/views/json/municipalityvleracmimi.py
@@ -1,18 +1,17 @@
-from flask import render_template, Response
+from flask import Response
 from flask.views import View
 from urllib2 import urlopen
 from gpv import utils
-import json
 
 
 class MunicipalityVleraCmimi(View):
     def dispatch_request(self, viti):
-    	api_base_url = utils.get_api_url()
+        api_base_url = utils.get_api_url()
         url = "%s/monthly-summary/%d" % (api_base_url, viti)
 
         result = urlopen(url).read()
 
-    	resp = Response(
+        resp = Response(
             response=result, mimetype='application/json')
         # Return response.
         return resp
--- gpv/views/pages/municipalityPriceValue.py
+++ gpv/views/pages/municipalityPriceValue.py
@@ -1,8 +1,8 @@
-from flask import render_template
+from flask import render_template, request
 from flask.views import View
 
 
 class MunicipalityPriceValue(View):
     def dispatch_request(self):
-        komuna = 'ferizaj'
-        return render_template('municipality_price_value.html', komuna=komuna)
+        commune = request.args.get('komuna')
+        return render_template('municipality_price_value.html', komuna=commune)
