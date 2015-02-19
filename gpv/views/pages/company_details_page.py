a78d8edb05da33d3e3b936b6a080525196c4d23e
Georges Labreche <georges@opendatakosovo.org>
2015-02-19 11:47:30 -0800

    work around for table style errors/conflicts

--- gpv/views/pages/company_details_page.py
+++ gpv/views/pages/company_details_page.py
@@ -4,7 +4,7 @@ from flask.views import View
 
 class CompanyDetailsPage(View):
     def dispatch_request(self, kompania_slug):
-    	commune = request.args.get('komuna')
+        commune = request.args.get('komuna')
         if commune == "None":
             commune = "ferizaj"
         return render_template('company_details.html', kompania_slug=kompania_slug, komuna=commune)
